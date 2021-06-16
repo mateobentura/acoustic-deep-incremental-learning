179cfb42cef784e6a0ef2fdaabeec2d5784dce5f
Mateo Bentura <mateo.bentura-larregui@imt-atlantique.net>
2021-06-16 12:17:18 +0200

    Changement vers approche avec des classes, images train+test

--- ImageSynthetique.py
+++ ImageSynthetique.py
@@ -12,87 +12,93 @@ plt.rcParams.update(params)
 
 import cv2
 
-def create_background(height, width, background=5):
-  image = np.ones((height,width), np.float32) * background
-  image = noisy(image, 10)
-  mask = np.zeros((height, width), np.uint8)
-  return image, mask
+def create_background(height, width, background=2):
+    image = np.ones((height,width), np.float32) * background
+    image = noisy(image, 10)
+    mask = np.zeros((height, width), np.uint8)
+    return image, mask
 
 
 def add_bars(image, mask, starting_pt, spacing, length, lines):
-  times = 4
-  height, width = image.shape
-  big_image = np.zeros((height*times,width*times), np.float32)
+    times = 4
+    height, width = image.shape
+    big_image = np.zeros((height*times,width*times), np.float32)
 
-  starting_pt *= times
-  spacing *= times
-  length *= times
-  lines = 15
+    starting_pt *= times
+    spacing *= times
+    length *= times
+    lines = 15
 
-  start = np.array(starting_pt - [length // 2, 0])
-  rect_top = tuple((start+[0,spacing])//times-2)
-  end = np.array(starting_pt + [length // 2, 0])
+    start = np.array(starting_pt - [length // 2, 0])
+    rect_top = tuple(start//times-2)
+    end = np.array(starting_pt + [length // 2, 0])
 
-  min_intensity = 50
-  for line in range(lines):
-      intensity = np.random.randint(min_intensity,200)
-      thickness = np.random.randint(times,2*times)
-      start[1] += spacing
-      end[1] += spacing
-      cv2.line(big_image, tuple(start), tuple(end), intensity, thickness)
+    min_intensity = 50
+    for line in range(lines):
+        intensity = np.random.randint(min_intensity,200)
+        thickness = np.random.randint(times,2*times)
+        cv2.line(big_image, tuple(start), tuple(end), intensity, thickness)
+        start[1] += spacing
+        end[1] += spacing
 
-  rect_bottom = tuple(end//times+2)
-  mask = cv2.rectangle(mask, rect_top, rect_bottom, 255, -1)
+    rect_bottom = tuple((end-[0,spacing])//times+2)
+    mask = cv2.rectangle(mask, rect_top, rect_bottom, 255, -1)
 
-  image_resize = cv2.resize(big_image,(width,height))
-  #image_resize = cv2.GaussianBlur(image_resize,(5,5),0.6)
-  mask_2 = image_resize.copy().astype(np.uint8)
-  _, mask_2 = cv2.threshold(mask_2, min_intensity, 255, cv2.THRESH_BINARY)
-  mask_2 += image_resize.astype(np.uint8)
-  mask_2 = cv2.bitwise_not(mask_2)
+    image_resize = cv2.resize(big_image,(width,height))
+    #image_resize = cv2.GaussianBlur(image_resize,(5,5),0.6)
+    mask_2 = image_resize.copy().astype(np.uint8)
+    _, mask_2 = cv2.threshold(mask_2, min_intensity, 255, cv2.THRESH_BINARY)
+    mask_2 += image_resize.astype(np.uint8)
+    mask_2 = cv2.bitwise_not(mask_2)
 
-  image_ret = cv2.bitwise_and(image, image, mask = mask_2)
-  image_ret = cv2.add(image_ret, image_resize)
-  return image_ret, mask
+    image_ret = cv2.bitwise_and(image, image, mask = mask_2)
+    image_ret = cv2.add(image_ret, image_resize)
+    return image_ret, mask, rect_top, rect_bottom
 
 def noisy(image, height):
-  row,col= image.shape
-  s_vs_p = 0.5
-  amount = 0.08
-  out = np.copy(image)
-  # Salt mode
-  num_salt = np.ceil(amount * image.size * s_vs_p)
-  coords = [np.random.randint(0, i - 1, int(num_salt))
+    row,col= image.shape
+    s_vs_p = 0.5
+    amount = 0.08
+    out = np.copy(image)
+    # Salt mode
+    num_salt = np.ceil(amount * image.size * s_vs_p)
+    coords = [np.random.randint(0, i - 1, int(num_salt))
           for i in image.shape]
-  out[tuple(coords)] += height
-  # Pepper mode
-  num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
-  coords = [np.random.randint(0, i - 1, int(num_pepper))
+    out[tuple(coords)] += height
+    # Pepper mode
+    num_pepper = np.ceil(amount* image.size * (1. - s_vs_p))
+    coords = [np.random.randint(0, i - 1, int(num_pepper))
           for i in image.shape]
-  out[tuple(coords)] -= height
-  return out
+    out[tuple(coords)] -= height
+    return out
 
 def sliding_window(image, image_mask, window_size, pad_h, pad_v):
-  windows_h = (width - window_size) // pad_h + 1
-  windows_v = (height - window_size) // pad_v + 1
-  crops = np.zeros((windows_v, windows_h, window_size, window_size))
-  labels = np.zeros((windows_v, windows_h))
-  for j in range(windows_v):
-    y_top = j*pad_v
-    y_bottom = j*pad_v + window_size
-    for i in range(windows_h):
-      x_top = i*pad_h
-      x_bottom = i*pad_h + window_size
-      crop = image[y_top:y_bottom, x_top:x_bottom]
-      crops[j,i,:,:] = crop
-      mask_crop = image_mask[y_top:y_bottom, x_top:x_bottom]
-      labels[j,i] = mask_crop.any()
-  return crops, labels
+    windows_h = (width - window_size) // pad_h + 1
+    windows_v = (height - window_size) // pad_v + 1
+    crops = np.zeros((windows_v, windows_h, window_size, window_size))
+    labels = np.zeros((windows_v, windows_h))
+    for j in range(windows_v):
+        y_top = j*pad_v
+        y_bottom = j*pad_v + window_size
+        for i in range(windows_h):
+            x_top = i*pad_h
+            x_bottom = i*pad_h + window_size
+            crop = image[y_top:y_bottom, x_top:x_bottom]
+            crops[j,i,:,:] = crop
+            mask_crop = image_mask[y_top:y_bottom, x_top:x_bottom]
+            labels[j,i] = mask_crop.any()
+    return crops, labels
 
 def plot_masked(image, mask):
-  masked = cv2.bitwise_and(image, image, mask=mask)
-  fig = plt.imshow(masked, vmin=0, vmax=255)
-  pass
+    masked = cv2.bitwise_and(image, image, mask=mask)
+    fig = plt.imshow(masked, vmin=0, vmax=255)
+    pass
+
+def plot_label(image, rect_top, rect_bottom):
+    temp = image.copy()
+    temp = cv2.rectangle(temp, rect_top, rect_bottom, 255, 1)
+    fig = plt.imshow(temp, vmin=0, vmax=255)
+    pass
 
 def resize_labels(labels, pad_h, pad_v, window_size):
   labels_resize = np.zeros_like(image, dtype='uint8')
--- main.py
+++ main.py
@@ -1,18 +1,100 @@
 from ImageSynthetique import *
+class Image(object):
+    """docstring for ."""
 
+    def __init__(self, height, width=640):
+        #super(Image, self).__init__()
+        self.height = height
+        self.width = width
+        self.create_image()
+        self.objects = []
+
+    def create_image(self):
+        self.image = np.ones((self.height, self.width), np.float32) * 2
+        self.image = noisy(self.image, 10)
+        self.mask = np.zeros((self.height, self.width), np.uint8)
+        pass
+
+    def add_object(self, starting_pt, spacing, length, lines):
+        times = 4
+        big_image = np.zeros((self.height*times,self.width*times), np.float32)
+
+        starting_pt = np.array(starting_pt)*times
+        spacing *= times
+        length *= times
+        lines = 15
+
+        start = np.array(starting_pt - [length // 2, 0])
+        rect_top = tuple(start//times-2)
+        end = np.array(starting_pt + [length // 2, 0])
+
+        min_intensity = 50
+        for line in range(lines):
+            intensity = np.random.randint(min_intensity,200)
+            thickness = np.random.randint(times,2*times)
+            cv2.line(big_image, tuple(start), tuple(end), intensity, thickness)
+            start[1] += spacing
+            end[1] += spacing
+
+        rect_bottom = tuple((end-[0,spacing])//times+2)
+        self.mask = cv2.rectangle(self.mask, rect_top, rect_bottom, 255, -1)
+
+        image_resize = cv2.resize(big_image,(self.width,self.height))
+        #image_resize = cv2.GaussianBlur(image_resize,(5,5),0.6)
+        mask_2 = image_resize.copy().astype(np.uint8)
+        _, mask_2 = cv2.threshold(mask_2, min_intensity, 255, cv2.THRESH_BINARY)
+        mask_2 += image_resize.astype(np.uint8)
+        mask_2 = cv2.bitwise_not(mask_2)
+
+        self.image = cv2.bitwise_and(self.image, self.image, mask = mask_2)
+        self.image = cv2.add(self.image, image_resize)
+        self.objects.append((rect_top,rect_bottom))
+        pass
+
+    def plot_masked(self):
+        masked = cv2.bitwise_and(self.image, self.image, mask=self.mask)
+        fig = plt.imshow(masked, vmin=0, vmax=255)
+        pass
+
+    def plot_label(self):
+        print(self.image.shape)
+        temp = self.image.copy()
+        for obj in self.objects:
+            temp = cv2.rectangle(temp, obj[0], obj[1], 255, 1)
+        fig = plt.imshow(temp, vmin=0, vmax=255)
+        return fig
+        
 
 height = 300
 width = 640
 
+train = Image(height, width)
 
-starting_point = np.array([300, 30])
+starting_point = [300, 20]
 spacing = 15
 length = 12
 lines = 15
 
-image, mask = create_background(height, width, background=2)
-image, mask = add_bars(image, mask, starting_point, spacing, length, lines)
+train.add_object(starting_point, spacing, length, lines)
+train.plot_label()
+plt.savefig('train')
 
-#plot_masked(image, mask)
-plt.imshow(image)
+# Image test
+test = Image(300)
+
+# Premier objet
+starting_point = [150, 20]
+spacing = 10
+length = 10
+lines = 8
+test.add_object(starting_point, spacing, length, lines)
+
+# Deuxième objet
+starting_point = [350, 200]
+spacing = 6
+length = 5
+lines = 8
+test.add_object(starting_point, spacing, length, lines)
+
+fig = test.plot_label()
 plt.savefig('test')
